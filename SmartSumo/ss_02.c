85b71b5bc15d695dc4a9ef2c3aff71d599a2a4ed
drAlexAK <alex.a.kudryashov@gmail.com>
2017-03-16 01:30:26 +0300

    [+] added sonar sensors and removed sumo sensor

--- SmartSumo/ss_02.c
+++ SmartSumo/ss_02.c
@@ -1,6 +1,5 @@
 #pragma config(Sensor, S1,     sSonarLeft,     sensorSONAR)
 #pragma config(Sensor, S2,     sSonarRight,    sensorSONAR)
-#pragma config(Sensor, S3,     sSumo,          sensorLightActive)
 #pragma config(Sensor, S4,     sLight,         sensorI2CCustom9V)
 #pragma config(Motor,  motorA,          mShLeft,       tmotorNXT, PIDControl, reversed, encoder)
 #pragma config(Motor,  motorB,          mLeft,         tmotorNXT, PIDControl, encoder)
@@ -15,17 +14,18 @@ int GetLightAverage();
 
 task main()
 {
-
+	sleep (3000);
 	motor[mShLeft]=100;									// shovel down
-	sleep(250);
-	motor[mShLeft]=5;										// shovel light pressure
+	sleep(200);
+	motor[mShLeft]=5;// shovel light pressure
 
-	MSSUMOsetLongRange(sSumo); 					// sets sumo sensor long range
+	MSSUMOsetLongRange(sSumo); 	// sets sumo sensor short range
+	bool SumoRangeLong = true ;
 	tObstacleZone zone = MSSUMO_NONE;		// sumo sensor result
 
-	const int sonarDistance  = 50;
-	int sonarLeft 					 = 0;				// current left sonar sensor
-	int sonarRight 					 = 0;				// current left sonar sensor
+	//const int sonarDistance  = 50;
+	//int sonarLeft 					 = 0;				// current left sonar sensor
+//	int sonarRight 					 = 0;				// current left sonar sensor
 	SensorValue(sSonarLeft)	 = 0;				// init left sonar sensor
 	SensorValue(sSonarRight) = 0;				// init right sonar sensor
 
@@ -42,76 +42,97 @@ task main()
 	{
 		lightCurrent = GetLightAverage();		// gt current light
 
-		if (abs(GetLightAverage() - lightStart) > 70) // U turn
+		if (abs(lightCurrent - lightStart) > 45) // U turn
 		{
 			motor[mLeft]  = -100; 	// move back
-			motor[mRight]= -100;		// move back
-			sleep(500);
+			motor[mRight] = -100;		// move back
+			sleep(300);
 			motor[mLeft]  =  50;		// turn
 			motor[mRight] = -50;		// turn
-			sleep(350);
+			sleep(250);
+
 		}
 		tObstacleZone zone = MSSUMOreadZone(sSumo); 	// get range from sume sensor
 
-		switch (zone) {
+		switch (zone)
+		{
 		case MSSUMO_FRONT: // go ahead
 			vLeft  = 100;
 			vRight = 100;
 			break;
 		case MSSUMO_LEFT: // turn left
-			vLeft  = -50;
-			vRight =  50;
+			vLeft  =  0;
+			vRight = -20;
 			break;
 		case MSSUMO_RIGHT: // turn right
-			vLeft  = 50;
-			vRight = -50;
+			vLeft  = -20;
+			vRight =  0;
 			break;
 		case MSSUMO_NONE:
-			// use sonar sensor here because sumo sensors returned nothing (none)
-			sonarLeft  = SensorValue(sSonarLeft);
-			sonarRight = SensorValue(sSonarRight);
-			if ( sonarLeft < sonarDistance )
+			MSSUMOsetLongRange(sSumo); 					// sets sumo sensor short range
+			bool SumoRangeLong = true ;
+			if ( SumoRangeLong == true )
 			{
-				if ( sonarRight < sonarDistance )  // go ahead
-				{
-					vLeft  = 100;
-					vRight = 100;
-				}
-				else 															// turn left
-				{
-					vLeft  = -50;
-					vRight =  50;
-				}
+				MSSUMOsetShortRange(sSumo);
+				SumoRangeLong = false;
 			}
 			else
 			{
-				if ( sonarRight < sonarDistance ) // turn right
-				{
-					vLeft  =  50;
-					vRight = -50;
-				}
+				MSSUMOsetLongRange(sSumo);
+				SumoRangeLong = true;
 			}
 			break;
 		}
 
-		motor[mLeft]=vLeft;
-		motor[mRight]=vRight;
+
+
+		// use sonar sensor here because sumo sensors returned nothing (none)
+		/*
+		sonarLeft  = SensorValue(sSonarLeft);
+		sonarRight = SensorValue(sSonarRight);
+		if ( sonarLeft < sonarDistance )
+		{
+		if ( sonarRight < sonarDistance )  // go ahead
+		{
+		vLeft  = 100;
+		vRight = 100;
+		}
+		else 															// turn left
+		{
+		vLeft  = -50;
+		vRight =  50;
+		}
+		}
+		else
+		{
+		if ( sonarRight < sonarDistance ) // turn right
+		{
+		vLeft  =  50;
+		vRight = -50;
+		}
+		}
+		*/
+		break;
 	}
+
+	motor[mLeft]=vLeft;
+	motor[mRight]=vRight;
 }
+
 
 // retruns light average from 0 - black to 100 - white
 int GetLightAverage()
 {
-	int average = 0;
-	int sensor = LLreadResult(sLight); // fetch the data from the sensor
-	tByteArray signalstr;
+int average = 0;
+//int sensor = LLreadResult(sLight); // fetch the data from the sensor
+tByteArray signalstr;
 
-	LLreadSensorRaw(sLight, signalstr); // read the raw sensor data (8 bit data)
+LLreadSensorRaw(sLight, signalstr); // read the raw sensor data (8 bit data)
 
-	for (int i = 0 ; i<8; i++)
-	{
-		average += signalstr[i];
-	}
-	average /= 8;
-	return average;
+for (int i = 0 ; i<8; i++)
+{
+	average += signalstr[i];
+}
+average /= 8;
+return average;
 }

1d67564962ab520da58fc3eec7e3086c8fbb9422
drAlexAK <alex.a.kudryashov@gmail.com>
2017-03-17 15:25:10 +0300

    [+] add proportion for go ahead

--- SmartSumo/ss_03.c
+++ SmartSumo/ss_03.c
@@ -8,28 +8,23 @@
 
 #include "mindsensors-lineleader.h"
 
-int GetLightAverage();
 bool IsHereWhite();
 
 task main()
 {
 	//-----------------------------------------------------
-	sleep (3000);
+	sleep (5000);
 	motor[mShLeft]=100;					// shovel down
 	sleep(200);
-	motor[mShLeft]=5;					// shovel light pressure
+	motor[mShLeft]=10;					  // shovel light pressure
 	//-----------------------------------------------------
 
 	//-----------------------------------------------------
-	const int sonarDistance  = 50;
-	int sonarLeft 					 = 0;	// current left sonar sensor
-	int sonarRight 					 = 0;	// current left sonar sensor
+	const int sonarDistance  = 55;
+	int sonarLeft 					 = 0;				// current left sonar sensor
+	int sonarRight 					 = 0;				// current left sonar sensor
 	SensorValue(sSonarLeft)	 = 0;				// init left sonar sensor
 	SensorValue(sSonarRight) = 0;				// init right sonar sensor
-	//-----------------------------------------------------
-
-	//-----------------------------------------------------
-	// int lightStart = GetLightAverage(); // remember initial light
 	//-----------------------------------------------------
 
 	// Initit motors
@@ -37,7 +32,7 @@ task main()
 	int vLeft 	= 100;					// left motor power
 	int vRight 	= 100;					// right motor power
 
-	motor[mLeft] = vLeft;					// go ahead
+	motor[mLeft]  = vLeft;					// go ahead
 	motor[mRight] = vRight;					// go ahead
 	//-----------------------------------------------------
 
@@ -45,80 +40,63 @@ task main()
 	{
 		// Get light sensor value and turn when current value less or more than start value
 		//-----------------------------------------------------
-		//if (abs(GetLightAverage() - lightStart) > 45) // U turn
 		if ( IsHereWhite() ) // U turn
 		{
 			motor[mLeft]  = -100; 		// move back
-			motor[mRight] = -100;		// move back
-			sleep(300);
-			motor[mLeft]  =  50;		// turn
-			motor[mRight] = -50;		// turn
+			motor[mRight] = -100;		  // move back
+			sleep(350);
+			motor[mLeft]  =  35;		  // turn
+			motor[mRight] = -35;		  // turn
 			sleep(250);
 		}
 		//-----------------------------------------------------
-
 
 		// use sonar sensor here
 		//-----------------------------------------------------
 		sonarLeft  = SensorValue(sSonarLeft);
 		sonarRight = SensorValue(sSonarRight);
 
+		if ( sonarLeft  < 5 ) sonarLeft  = 255; // protects from stupid sonar issue when value is 0
+		if ( sonarRight < 5 ) sonarRight = 255; // protects from stupid sonar issue when value is 0
+
 		if ( sonarLeft < sonarDistance ) // see on the left
 		{
 			if ( sonarRight < sonarDistance ) // see on the right too. Go ahead
 			{
-				vLeft  = 100;
-				vRight = 100;
+				vLeft  = 100 * sonarLeft / sonarRight ;
+				vRight = 100 * sonarRight / sonarLeft ;
 			}
 			else // see only left only. Turn left
 			{
-				vLeft  =  0;
-				vRight = -20;
+				vLeft  =  15;
+				vRight = -15;
 			}
 		}
 		else
 		{
 			if ( sonarRight < sonarDistance ) // see on the right only. Turn right
 			{
-				vLeft  = -20;
-				vRight =  0;
+				vLeft  = -15;
+				vRight =  15;
 			}
 		}
+
+		// sets motors power value
+		motor[mLeft]  = vLeft;
+		motor[mRight] = vRight;
+		sleep(1);
 	}
-	// sets motors power value
-	motor[mLeft]=vLeft;
-	motor[mRight]=vRight;
 }
 
-
-// retruns light average from 0 - black to 100 - white
-int GetLightAverage()
-{
-	int average = 0;
-	int sensor = LLreadResult(sLight); // fetch the data from the sensor
-	tByteArray signalstr;
-
-	LLreadSensorRaw(sLight, signalstr); // read the raw sensor data (8 bit data)
-
-	for (int i = 0 ; i<8; i++)
-	{
-		average += signalstr[i];
-	}
-	average /= 8;
-	return average;
-}
-
-// returns true when value of anysensor more than 70
+// returns true when value of anysensor more than 25
 bool IsHereWhite()
 {
-	int sensor = LLreadResult(sLight); // fetch the data from the sensor
 	tByteArray signalstr;
-
 	LLreadSensorRaw(sLight, signalstr); // read the raw sensor data (8 bit data)
 
 	for (int i = 0 ; i < 8; i++)
 	{
-		if ( signalstr[i] > 70) return true;
+		if ( signalstr[i] > 35 ) return true;
 	}
 	return false;
 }

ec4e7eaad6b19d9e95e310ee2bfcd730106c8843
drAlexAK <alex.a.kudryashov@gmail.com>
2017-06-02 12:29:59 +0300

    [+]

--- Project activity/SmartTraffic.c
+++ Project activity/SmartTraffic.c
@@ -23,17 +23,17 @@ task main()
 	//sleep(2000);
 
 	//----------------------------------
-	int es = SensorValue(sLightLeft) - SensorValue(sLightRight);
-	int e     = 0;
-	int eOld  = e;
-	int u     = 0;
-	int const vMax     = 80;
-	int const vMin   = 0;
-	int v = vMax;
-	int i     = 0;
-  int s = SensorValue(Sonar);
-  int vLeft =0;
-  int vRight =0;
+	int es 					= SensorValue(sLightLeft) - SensorValue(sLightRight);
+	int e     			= 0;
+	int eOld  			= e;
+	int u     			= 0;
+	int const vMax  = 80;
+	int const vMin  = 0;
+	int v 					= vMax;
+	int i     			= 0;
+	int s 					= SensorValue(Sonar);
+	int vLeft 			= 0;
+	int vRight 			= 0;
 	//----------------------------------
 	e  = SensorValue(sLightLeft) - SensorValue(sLightRight) - es;
 	eOld = e;
@@ -42,21 +42,21 @@ task main()
 	{
 		s = SensorValue(Sonar);
 		//-------------------------
-		if(s > 40) v = vMax;
-		else if(s > 35) v = vMax - 10;
-		else if(s > 30) v = vMax - 20;
-		else if(s > 25) v = vMax - 30;
-		else if(s > 20) v = vMax - 50;
-		else if(s > 15) v = vMax - 60;
-		else v = vMin ;
+		if ( s > 40 )      v = vMax;
+		else if ( s > 35 ) v = vMax - 10;
+		else if ( s > 30 ) v = vMax - 20;
+		else if ( s > 25 ) v = vMax - 30;
+		else if ( s > 20 ) v = vMax - 45;
+		else if ( s > 15 ) v = vMax - 60;
+		else               v = vMin ;
 		//-------------------------
 		if((SensorValue(sLightLeft) < 30) && (SensorValue(sLightRight) < 30))
 		{
 			i++ ;
-			if( i % 2 != 0)
+			if( i % 2 != 0 )
 			{
 				motor[mLeft]  = v / 2 ;
-				motor[mRight] = v / 2;
+				motor[mRight] = v / 2 ;
 				//SensorType[sColor] = sensorEV3_Color;
 				//SensorMode[sColor] = modeEV3Color_Color;
 			}
@@ -75,6 +75,7 @@ task main()
 					if(getColorName(sColor) == colorGreen) break;
 					motor[mLeft]  = 0;
 					motor[mRight] = 0;
+					sleep(10);
 					//SensorType[sLightLeft] = sensorLightInactive;
 					//SensorType[sLightRight] = sensorLightInactive;
 				}
@@ -89,15 +90,16 @@ task main()
 			e = SensorValue(sLightLeft) - SensorValue(sLightRight) - es;
 			u = e * 1.5 + (e - eOld) * 5;
 			eOld = e;
+			vLeft = v - u;
+			vRight = v + u;
 			//-------------------------
-		vLeft = v - u;
-		vRight = v + u;
-		if(vLeft < vMin)vLeft = vMin;
-		if(vRight < vMin)vRight = vMin;
-		if(vLeft > vMax)vLeft = vMax;
-		if(vRight > vMax)vRight = vMax;
-		motor[mLeft] = vLeft;
-		motor[mRight] = vRight;
+
+			if ( vLeft  < vMin ) vLeft  = vMin;
+			if ( vRight < vMin ) vRight = vMin;
+			if ( vLeft  > vMax ) vLeft  = vMax;
+			if ( vRight > vMax ) vRight = vMax;
+			motor[mLeft]  = vLeft;
+			motor[mRight] = vRight;
 		}
 		sleep(10);
 	}

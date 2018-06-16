4736ea198e145771ce8e2814b36f24672800b349
drAlexAK <alex.a.kudryashov@gmail.com>
2018-06-16 17:56:34 +0300

    [-] fixed bugs

--- ITMO/robotAppleBot/platform.c
+++ ITMO/robotAppleBot/platform.c
@@ -188,6 +188,10 @@ bool catchApple(){
 		//if(!getCoord(y, x)) break;
 		if (camStatus.Apple == false) break;
 		sendCommand(CMD_SHIFT_PL_MM, shiftPL);
+		// here----------------------------------------------------------------------------------------
+		sendCommand(CMD_SAVE_ARM_MM);
+		sendCommand(CMD_RESTORE_ARM_MM, 5);
+		//---------------------------------------------------------------------------------------------
 		sendCommand(CMD_DOWN_LANDLE, 0);
 		sendCommand(CMD_SHIFT_PL_MM, -1 * shiftPL);
 		sleep(100);
